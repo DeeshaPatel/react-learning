f3ba57cd4fbc316ecbb61bf5dfc289f7c87de382
DeeshaPatel <pateldeesha99@gmail.com>
2024-01-21 18:20:04 -0500

    Buttons Implementation

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,15 +1,40 @@
 import React from "react";
-import { Text, StyleSheet } from "react-native";
+import { Text, StyleSheet, View, Button, TouchableOpacity } from "react-native";
 
-const HomeScreen = () => {
-  return <Text style={styles.text}>React</Text>;
+const HomeScreen = ({navigation}) => {
+
+  return (
+  <View>
+    <Text style={styles.text}>React</Text>
+
+    <Button 
+      color="#f194ff" 
+      title="Go to Components Demo" 
+      onPress={() => navigation.navigate('Components') } 
+    />
+    
+    <TouchableOpacity 
+      style={styles.buttonText}
+      onPress={() => navigation.navigate('List')} >
+        <Text style={styles.buttonText}>Go to List Demo</Text>
+    </TouchableOpacity>
+
+  </View>
+  );
 };
-
+  
 const styles = StyleSheet.create({
   text: {
     fontSize: 20,
-    
   },
+  buttonText: {
+    fontSize: 20,
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 2,
+    margin:10,
+    borderRadius: 20,
+  }
 });
 
 export default HomeScreen;
