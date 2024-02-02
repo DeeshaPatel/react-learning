71acbc8629179000f550f35fe90a65e53a7896c5
DeeshaPatel <pateldeesha99@gmail.com>
2024-02-02 14:52:11 -0500

    Reducer in Inc and dec exercise

--- src/screens/CounterScreen.js
+++ src/screens/CounterScreen.js
@@ -1,22 +1,49 @@
-import React, { useState } from "react";
+import React, { useReducer } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
+
+const reducer = (state, action) => {
+    // state === {count: 0} 
+    // action === { type: 'inc' || 'dec', payload: 1}
+
+    switch(action.type) {
+        case 'inc':
+            return {...state, count: state.count + action.payload};
+        case 'dec':
+            return {...state, count: state.count - action.payload};
+        default:
+            return state;
+
+    }
+}
 
 const CounterScreen = () => {
 
-    const [counter, setCounter] = useState(0);
+    // const [counter, setCounter] = useState(0);
+    // use of reducer for practice. 
+    const [state, dispatch] = useReducer(reducer, {count: 0});
 
-    const increseCounter = () => {
-        setCounter(counter + 1);
-    }
-    const decreseCounter = () => {
-        setCounter(counter + 1);
-    }
+    // const increseCounter = () => {
+    //     setCounter(counter + 1);
+    // }
+    // const decreseCounter = () => {
+    //     setCounter(counter + 1);
+    // }
 
     return (
         <View>
-            <Button title="Increse" onPress={increseCounter}/>
-            <Button title="Decrease" onPress={decreseCounter}/>
-            <Text>Current Count: {counter}</Text>
+            {/* <Button title="Increse" onPress={increseCounter}/>
+            <Button title="Decrease" onPress={decreseCounter}/> */}
+            <Button title="Increse" 
+                onPress={() => {
+                    dispatch({ type: 'inc', payload: 1})
+                }}
+             />
+            <Button title="Decrease" 
+                 onPress={() => {
+                    dispatch({ type: 'dec', payload: 1})
+                }}
+            />
+            <Text>Current Count: {state.count}</Text>
         </View>
     )
 }
